4fef44022c22926c8916a080c3e53098edae5a1d
Leo <leo.pratama@aqi.co.id>
2024-09-02 11:33:19 +0700

    add: break point for responsive, animation scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,15 +4,15 @@ const navMenu = document.getElementById('nav-menu'),
     navClose = document.getElementById('nav-close')
 
 // show menu
-if(navToggle){
-    navToggle.addEventListener('click', () =>{
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
 //hidden menu
-if(navClose){
-    navClose.addEventListener('click', () =>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -20,7 +20,7 @@ if(navClose){
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
-const linkAction = () =>{
+const linkAction = () => {
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
@@ -28,33 +28,71 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const bgHeader = () =>{
+const bgHeader = () => {
     const header = document.getElementById('header')
-    if(this.scrollY >= 50) header.classList.add('bg-header')
+    if (this.scrollY >= 50) header.classList.add('bg-header')
     else header.classList.remove('bg-header')
 }
 
 window.addEventListener('scroll', bgHeader)
 bgHeader()
 
-/*=============== SWIPER SERVICES ===============*/ 
+/*=============== SWIPER SERVICES ===============*/
 const swiperService = new Swiper('.services__swiper', {
     loop: true,
     grabCursor: true,
     spaceBetween: 24,
     slidesPerView: 'auto',
-  
+
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  });
+});
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
+const section = document.querySelectorAll('section[id]')
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll')
+    else scrollUp.classList.remove('show-scroll')
+}
 
+window.addEventListener('scroll', scrollUp)
+scrollUp()
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const scrollActive = () => {
+    const scrollDown = window.scrollY
 
+    section.forEach(current => {
+        const sectionTop = current.offsetTop - 58
+        const sectionHeight = current.offsetHeight
+        const sectionId = current.getAttribute('id')
+        const sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if (scrollDown > sectionTop && scrollDown < sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '100px',
+    duration: 2500,
+    delay: 400,
+    // reset: true,
+})
+
+sr.reveal(`.home__content, .services__data, .services__swiper, .footer__container`)
+sr.reveal(`.home__images`, { delay: 1000, origin: 'bottom' })
+sr.reveal(`.about__images, .contact__img`, { origin: 'left' })
+sr.reveal(`.about__data, .contact__data`, { origin: 'right' })
+sr.reveal(`.projects__card`, { interval: 100})
